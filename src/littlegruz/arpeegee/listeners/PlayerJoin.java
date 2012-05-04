027af9c237cf53cbe508f45b92cd021e47c7a1b5
unknown <littlegruz@gmail.com>
2012-05-04 19:13:14 +1000

    Intro text improved

--- src/littlegruz/arpeegee/listeners/PlayerJoin.java
+++ src/littlegruz/arpeegee/listeners/PlayerJoin.java
@@ -2,6 +2,7 @@ package littlegruz.arpeegee.listeners;
 
 import littlegruz.arpeegee.ArpeegeeMain;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,14 +22,15 @@ public class PlayerJoin implements Listener{
       if(plugin.getMeleePlayerMap().get(event.getPlayer().getName()) == null
             && plugin.getRangedPlayerMap().get(event.getPlayer().getName()) == null
             && plugin.getMagicPlayerMap().get(event.getPlayer().getName()) == null){
-         playa.sendMessage("Welcome, " + playa.getName() + ", to LittleRPG.");
-         playa.sendMessage("Before you begin your adventure, you should pick a class.");
-         delayMessage(playa, "For those who wish to be able to converse with their prey, the melee class would be suitable. Type: '/iammelee'", 150);
-         delayMessage(playa, "If you would rather pick enemies off from a perch where they can not hit you, the ranged class is most appropriate. Type: '/iamranged'", 300);
-         delayMessage(playa, "But if spewing arcane energies from your fingertips appeals to you, the magic class is your best bet. Type: '/iammagic'", 450);
+         event.setJoinMessage(ChatColor.RED + "Welcome, " + playa.getName() + ", to LittleRPG.");
+         
+         delayMessage(playa, ChatColor.YELLOW + "Before you begin your adventure, you should pick a class.", 5);
+         delayMessage(playa, ChatColor.GREEN + "If you are one that desires to get close enough to taunt their prey, the melee class would be most suitable. Type: '/iammelee'", 150);
+         delayMessage(playa, ChatColor.BLUE + "If you would rather pick enemies off from a perch where they can not hit you, the ranged class is most appropriate. Type: '/iamranged'", 350);
+         delayMessage(playa, ChatColor.LIGHT_PURPLE + "But if spewing arcane energies from your fingertips appeals  to you best, then the magic class is your best bet. Type: '/iammagic'", 500);
       }
       else{
-         playa.sendMessage("Welcome back, brave adventurer.");
+         event.setJoinMessage("Welcome back, brave adventurer.");
       }
    }
    
