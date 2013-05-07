9015744dc44f13a3ba156ececdab7c571e1cf83e
littlegruz <littlegruz@gmail.com>
2013-05-07 11:29:06 +1000

    Player chat listener implemented

---
+++ src/littlegruz/arpeegee/listeners/PlayerChat.java
@@ -0,0 +1,51 @@
+package littlegruz.arpeegee.listeners;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import littlegruz.arpeegee.ArpeegeeMain;
+import littlegruz.arpeegee.entities.RPGPlayer;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerChat implements Listener{
+   private ArpeegeeMain plugin;
+   
+   public PlayerChat(ArpeegeeMain instance){
+      plugin = instance;
+   }
+
+   @EventHandler
+   public void onPlayerChat(AsyncPlayerChatEvent event){
+      if(plugin.getWorldsMap().containsKey(event.getPlayer().getWorld().getName())){
+         RPGPlayer rpgp;
+         String party;
+         
+         rpgp = plugin.findRPGPlayer(event.getPlayer().getName());
+         
+         if(rpgp != null && rpgp.getChat() == 1){
+            party = rpgp.getParty();
+            
+            if(party.compareTo("none") != 0){
+               Iterator<Map.Entry<String, String>> it = plugin.getPartyMap().get(party).getMembers().entrySet().iterator();
+               String msg = event.getPlayer().getName() + ": " + event.getMessage();//TODO format message
+               
+               /* Send the message to everyone but the sender in the party*/
+               while(it.hasNext()){
+                  Entry<String, String> playa = it.next();
+                  plugin.getServer().getPlayer(playa.getKey()).sendMessage(msg);
+               }
+               
+               event.setCancelled(true);
+            }
+            else{
+               event.getPlayer().sendMessage("You must join a party first");
+               event.setCancelled(true);
+            }
+         }
+      }
+   }
+}
